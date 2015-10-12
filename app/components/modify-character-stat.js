05dfadce8c3639ea9360abe994780ed60e967ced
kmonson <kirsten.monson@hope.edu>
2015-10-12 14:31:50 +0000

    shows value for titles

--- app/components/modify-character-stat.js
+++ app/components/modify-character-stat.js
@@ -1,20 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  characters: Ember.computed.alias('model'),
-  character: Ember.computed.alias('characters.firstObject'),
-    _modifyStat: function(stat, amount){
-    this.set('char.'+stat, this.get('char.'+stat)+amount);
+
+    _modifyStat: function(title, amount){
+    this.set('character.'+title, this.get('character.'+title)+amount);
   },
 
   actions: {
 
-  increaseStat: function(stat, char) {
-      this._modifyStat(stat, 1, char);
+  increaseStat: function(title) {
+      this._modifyStat(title, 1);
     },
 
-    decreaseStat: function(stat) {
-      this._modifyStat(stat, -1);
+    decreaseStat: function(title) {
+      this._modifyStat(title, -1);
     },
   }
 });
