3d4f10395ffb9065531aa603b1112a91e01929a3
kmonson <kirsten.monson@hope.edu>
2015-11-18 02:16:05 +0000

    adds a sample RSVP promise that wraps a sample ajax request

--- app/routes/authenticated/character.js
+++ app/routes/authenticated/character.js
@@ -16,6 +16,21 @@ export default Ember.Route.extend({
         resolve(charPromise);
       }, 3000);
     });
+  },
+
+   ajax: function(url, type, hash) {
+    var adapter = this;
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      hash = adapter.ajaxOptions(url, type, hash);  // this is your requestObj
+      hash.success = function(json) {
+        Ember.run(null, resolve, json);
+      };
+      hash.error = function(jqXHR, textStatus, errorThrown) {
+        Ember.run(null, reject, adapter.ajaxError(jqXHR));
+      };
+      Ember.$.ajax(hash); // this is the same thing as $.ajax
+    }, "DS: RestAdapter#ajax " + type + " to " + url); // they name their promise
   }
+
 });
 
