93ee9896992f8d4e4f589ce60b60a8892f312c9f
kmonson <kirsten.monson@hope.edu>
2015-11-18 00:52:26 +0000

    create loading page for characters

--- app/routes/authenticated/character.js
+++ app/routes/authenticated/character.js
@@ -1,7 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  /*model: function() {
+    var char = this.store.findAll('character');
+    return new Ember.RSVP.Promise(function(resolve) {
+      Ember.run.later(function() {
+        resolve({ msg: "Hold Your Horses" });
+      }, 3000);
+    }).then(function() { return char;});*/
+
   model: function() {
-    return this.store.findAll('character');
+    let charPromise = this.store.findAll('character');
+    return new Ember.RSVP.Promise(resolve => {
+      return Ember.run.later(() => {
+        resolve(charPromise);
+      }, 3000);
+    });
   }
 });
+
